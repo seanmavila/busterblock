047a31e18118d3b1fc10b84dd0d2efa8416b64d5
SkaiCow <WillALasater@gmail.com>
2020-05-02 16:05:27 -0500

    Will branch up to date

--- core/pool.js
+++ core/pool.js
@@ -6,10 +6,11 @@ const mysql = require("mysql");
 const pool = mysql.createPool({
   connectionLimiit: 10,
   host: "localhost",
-  user: "root", // your mysql username
-  password: "SMA8320sma1994$", //your mysql password
+  user: "cowcow", // your mysql username
+  password: "CowsGoMoo", //your mysql password
   database: "busterblock"
 });
+
 
 pool.getConnection((err, connection) => {
   if (err) console.error("Something went wrong connecting to the database ...");
