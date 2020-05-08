3a976516c57281fa5655ec6dae1483a4c9ac1042
seanmavila <sean.avila@live.com>
2020-05-08 00:02:52 -0500

    search change

--- core/movies.js
+++ core/movies.js
@@ -34,6 +34,7 @@ Movies.prototype = {
       }
     });
   },
+
   rentMovies: function (userID, list, callback) {
     let sql = `INSERT INTO rentals (uid, mid, rented) VALUES `;
     let rentDate = new Date();
@@ -65,13 +66,8 @@ Movies.prototype = {
   },
 
   find: function (movie = null, callback) {
-    // if the user variable is defind
-    if (movie) {
-      // if user = number return field = id, if user = string return field = username.
-      var field = Number.isInteger(movie) ? "mid" : "title";
-    }
     // prepare the sql query
-    let sql = `SELECT * FROM movies WHERE ${field} like ?`;
+    let sql = `SELECT * FROM movies WHERE title like ?`;
 
     pool.query(sql, movie, function (err, result) {
       if (err) throw err;
